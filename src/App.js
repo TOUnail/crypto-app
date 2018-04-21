0c25ee86bd7f751e491ab68dee8f5e51c3a1d80d
alextou <a.tou287@gmail.com>
2018-04-21 16:50:55 -0700

    merge both api requests using object.assign

--- src/App.js
+++ src/App.js
@@ -1,43 +1,44 @@
 import React, { Component } from 'react';
 import './App.css';
-import Price from './components/Price/Price';
 import axios from './crypto-compare.js';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      crypto: []
+      cryptos: {}
     }
   }
   componentDidMount() {
     Promise.all([
       //https://min-api.cryptocompare.com/data/top/exchanges/full?fsym=BTC&tsym=USD
       axios.get('data/coin/generalinfo?fsyms=BTC,MLN,DASH&tsym=USD'),
-      axios.get('data/pricemultifull?fsyms=BTC,MLN,DASH&tsyms=USD')
+      axios.get('data/pricemulti?fsyms=BTC,MLN,DASH&tsyms=USD')
     ])
     .then(([generalInfo, pricemulti]) => {
-      let cryptoData = generalInfo.data.Data.concat(pricemulti.data);
+      let cryptoData = generalInfo.data.Data;
+      let cryptoPrice = pricemulti.data;
+      let arrCryptoPrice = Object
+        .keys(cryptoPrice)
+        .map(function (key) {
+          return {[key]: cryptoPrice[key]};
+        });
+      for(var i = 0; i < cryptoData.length; i++) {
+        Object.assign(cryptoData[i].CoinInfo,arrCryptoPrice[i][cryptoData[i].CoinInfo.Name]);
+      }
       this.setState({
-        crypto:cryptoData
+        cryptos: cryptoData
       });
-      console.log(this.state.crypto);
     });
   }
 
   render() {
     return(
       <div className="App">
-        {Object.keys(this.state.crypto.slice(0,-1)).map((key) => (
+        {Object.keys(this.state.cryptos).map((key) => (
           <div id="crypto-container" className="container" key={key}>
-            <span className="left">
-              {this.state.crypto[key].CoinInfo.Name}
-              <img src={'https://www.cryptocompare.com' + this.state.crypto[key].CoinInfo.ImageUrl} alt="" className="crypto-img" />
-            </span>
-            <span className="right">
-              {this.state.crypto.slice(-1)[0].RAW.BTC.USD.FROMSYMBOL}
-              <Price />
-            </span>
+            <span className="left">{this.state.cryptos[key].CoinInfo.Name}</span>
+            <span className="right">{this.state.cryptos[key].CoinInfo.USD}</span>
           </div>
         ))}
       </div>
