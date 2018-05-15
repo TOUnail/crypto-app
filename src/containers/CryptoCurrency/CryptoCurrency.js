4482df47f820457ef6463f7e1fa273ca06d5c42a
alextou <a.tou287@gmail.com>
2018-05-15 01:37:18 -0700

    add chart to currency page

--- src/containers/CryptoCurrency/CryptoCurrency.js
+++ src/containers/CryptoCurrency/CryptoCurrency.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
+import './CryptoCurrency.css';
 import axios from '../../crypto-compare.js';
-import {VictoryLine,VictoryChart,VictoryTheme} from 'victory';
+import {VictoryLine,VictoryChart,VictoryAxis,VictoryTheme} from 'victory';
 
 class CryptoCurrency extends Component {
 	constructor(props) {
@@ -26,16 +27,80 @@ class CryptoCurrency extends Component {
 	        console.log(cryptos);
 		})*/
 	}
+	timeConverter(unix_time){
+		let a = new Date(unix_time * 1000);
+		let months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+		let year = a.getFullYear();
+		let month = months[a.getMonth()];
+		let date = a.getDate();
+
+		let hour = a.getHours();
+		let min = a.getMinutes();
+		let time =  + hour + ':' + min + '\n' + month + ' ' + date + ', ' + year;
+		return time;
+	}
 	render() {
 		return (
 			<div>
 				{this.props.match.params.id}
-				<div>
-					<VictoryChart theme={VictoryTheme.material}>
+				<div className="chart">
+					<VictoryChart theme={VictoryTheme.material} height={200}>
+						<VictoryAxis
+							tickFormat = {(y)=>(this.timeConverter(y))}
+							style = {{
+								tickLabels: {fontSize: 3}
+							}}
+						/>
+						<VictoryAxis
+							dependentAxis
+							tickFormat = {(x)=>(`$${x.toFixed(2)}`)}
+							style = {{
+								tickLabels: {fontSize: 5}
+							}}
+						/>
 						<VictoryLine
+							style={{
+						      data: {
+						      	stroke: "#c43a31",
+						      	strokeWidth: 0.5
+						      }
+						    }}
 							data={this.state.cryptoInfo}
 							x="time"
 							y="high"
+						/>
+						<VictoryLine
+							style={{
+						      data: {
+						      	stroke: "#ccc",
+						      	strokeWidth: 0.5
+						      }
+						    }}
+							data={this.state.cryptoInfo}
+							x="time"
+							y="low"
+						/>
+						<VictoryLine
+							style={{
+						      data: {
+						      	stroke: "#00ff00",
+						      	strokeWidth: 0.5
+						      }
+						    }}
+							data={this.state.cryptoInfo}
+							x="time"
+							y="open"
+						/>
+						<VictoryLine
+							style={{
+						      data: {
+						      	stroke: "#0000ff",
+						      	strokeWidth: 0.5
+						      }
+						    }}
+							data={this.state.cryptoInfo}
+							x="time"
+							y="close"
 						/>
 					</VictoryChart>
 				</div>
